9584faf01b0ae6cf6aeaeacdf91c2c8af035062d
Andrew Marks <ajmarks@gmail.com>
2020-08-12 09:48:05 -0400

    Maintainability tweak to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
 import pathlib
 from setuptools import setup
 
+try:
+    import re2 as re
+except ImportError:
+    import re
+
+packages = ["sharkiqpy"]
+
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-# This call to setup() does all the work
+# Pull the version from __init__.py so we don't need to maintain it in multiple places
+init_txt = (HERE / packages[0] / "__init__.py").read_text("utf-8")
+try:
+    version = re.findall(r"^__version__ = ['\"]([^'\"]+)['\"]\r?$", init_txt, re.M)[0]
+except IndexError:
+    raise RuntimeError('Unable to determine version.')
+
+
 setup(
     name="sharkiqpy",
-    version="0.1.8",
+    version=version,
     description="Python API for Shark IQ robots",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -23,7 +37,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["sharkiqpy"],
+    packages=packages,
     include_package_data=False,
     install_requires=["aiohttp", "requests"],
 )
